c3d2f813c939ab7078a3f151aaa703d2f36f0f9e
Yun Yo-han <dbsdy1235@gmail.com>
2021-07-29 16:31:33 +0900

    Update yohda_vim_env.sh

--- yohda_vim_env.sh
+++ yohda_vim_env.sh
@@ -4,3 +4,14 @@
 ln -s $(pwd)/.vimrc ~/.vimrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+#mkdir ~/code_search
+#cd ~/code_search
+
+#mkdir ctags
+#cd ctags
+#wget https://sourceforge.net/projects/ctags/files/ctags/5.8/ctags-5.8.tar.gz
+
+#mkdir cscope
+#cd ../cscope
+#wget https://sourceforge.net/projects/cscope/files/cscope/v15.9/cscope-15.9.tar.gz
